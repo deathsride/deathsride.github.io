6b8a69e76b933835c8049ae5a14dd7e6fc0b3613
cetiniz <zcetinic@quartermaster.house>
2020-03-20 17:24:37 -0400

    [Scripts] Add script to hide text

--- js/script.js
+++ js/script.js
@@ -9,16 +9,30 @@ const IMAGE_NAMES = [
     'image_4'
 ];
 
+const TEXT_NAMES = [
+    'copy-1',
+    'copy-2',
+    'copy-3',
+    'copy-4'
+];
+
 var expandScreen = function(event, selectedImageName) {
     event.stopPropagation();
+
     const selectedDiv = document.getElementsByClassName(selectedImageName);
+    const isFullScreen = selectedDiv[0].style.width === "100%"
+
+    for (const textName of TEXT_NAMES) {
+        const pTag = document.getElementsByClassName(textName)[0];
+        pTag.style.visibility = isFullScreen ? "visible" : "hidden";
+    }
+
     for (const imageName of IMAGE_NAMES) {
         if (selectedImageName == imageName) {
-            const width = selectedDiv[0].style.width;
-            selectedDiv[0].style.width = width === "100%" ? "25%" : "100%";
+            selectedDiv[0].style.width = isFullScreen ? "25%" : "100%";
         } else {
             const otherDiv = document.getElementsByClassName(imageName)[0];
-            otherDiv.style.width = otherDiv.style.width === "0%" ? "25%" : "0%";
+            otherDiv.style.width = isFullScreen ? "25%" : "0%";
         }
     }
 };
